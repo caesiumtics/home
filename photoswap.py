ed5ec0bd9286713b84b0cc8345209ec8a8caa089
caesiumtics <79468579+caesiumtics@users.noreply.github.com>
2021-03-03 15:13:02 -0600

    Update photoswap.py

--- photoswap.py
+++ photoswap.py
@@ -4,27 +4,21 @@ import math
 import pandas as pd
 import streamlit as st
 
+# load and show an image with Pillow
+from PIL import Image
+
 """
 # Simple Streamlit app by KS
 """
 
 with st.echo(code_location = 'below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 1000)
-    num_turns = st.slider("Number of turns in spiral", 1, 20, 5)
+    total_points = st.slider("Show image?", 0, 1, 0)
     
-    Point = namedtuple('Point', 'x y')
-    data = []
-    
-    points_per_turn = total_points / num_turns
-    
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-        
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height = 500, width = 500)
-                    .mark_circle(color='#0068c9', opacity = 0.5)
-                    .encode(x = 'x:Q', y = 'y:Q'))
+    # Open the image form working directory
+    image = Image.open('kolala.jpeg')
+    # summarize some details about the image
+    print(image.format)
+    print(image.size)
+    print(image.mode)
+    # show the image
+    load_image.show()
