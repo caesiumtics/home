d9990e56835b1da639193427aa2bfa7aa0c20cf7
caesiumtics <79468579+caesiumtics@users.noreply.github.com>
2021-03-03 15:00:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import streamlit as st
 
 with st.echo(code_location = 'below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 1000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    num_turns = st.slider("Number of turns in spiral", 1, 20, 9)
     
     Point = namedtuple('Point', 'x y')
     data = []
