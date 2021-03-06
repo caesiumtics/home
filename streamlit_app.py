eab82e41bf3fafa30c12b171f94082755edff2e0
caesiumtics <79468579+caesiumtics@users.noreply.github.com>
2021-03-05 21:20:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ import streamlit as st
 """
 # Simple Streamlit app by KS
 """
+st.write("About me: [here](https://www.linkedin.com/in/ksambath/)")
 
 with st.echo(code_location = 'below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 1000)
